dca441a492ff5e45c028b5c982cebcccd4c2599a
pmaksimov <vur21@ya.ru>
2021-02-23 18:42:52 +0300

    new version

--- datagun/__init__.py
+++ datagun/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Pavel Maksimov'
 __email__ = 'vur21@ya.com'
-__version__ = '2020.4.12'
+__version__ = '2021.2.10'
 
 from .datagun import Series, DataSet, read_text, deserialize_list, Gun, NULL_VALUES
